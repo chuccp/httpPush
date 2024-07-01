c3ebce7b4b4812ec0270b277e10ead09356f1f5c
tomcat <cooge123@gmail.com>
2024-07-01 13:09:22 +0800

    Update net_test.go

--- util/net_test.go
+++ util/net_test.go
@@ -1,22 +1,9 @@
 package util
 
 import (
-	"context"
-	"log"
 	"testing"
-	"time"
 )
 
 func TestName(t *testing.T) {
-	queue := NewQueue()
 
-	for {
-		ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-		v, flag := queue.Dequeue(ctx)
-		if flag {
-			log.Println(v)
-		} else {
-			cancelFunc()
-		}
-	}
 }
