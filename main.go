01c70b5504581bfc07735e78a5960e071e401fe5
tomcat <cooge123@gmail.com>
2026-06-30 14:45:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/chuccp/go-web-frame/config"
 	wflog "github.com/chuccp/go-web-frame/log"
 	"github.com/chuccp/httpPush/api"
+	"github.com/chuccp/httpPush/auth"
 	"github.com/chuccp/httpPush/cluster"
 	"github.com/chuccp/httpPush/core"
 	"github.com/chuccp/httpPush/ex"
@@ -27,6 +28,7 @@ func main() {
 	builder.Rest(ex.NewController())
 	builder.Rest(ws.NewController())
 	builder.Filter(cors.NewCrosFilter())
+	builder.Filter(auth.NewAuthFilter())
 	if err := builder.Build().Run(context.Background()); err != nil {
 		wflog.Fatal("server stopped", zap.Error(err))
 	}
